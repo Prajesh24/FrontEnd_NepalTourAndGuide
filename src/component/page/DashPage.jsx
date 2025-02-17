7953b914d78c0320345a76054c56d922f9fa59c4
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2025-02-17 18:43:34 +0545

    adding DashBoard page

---
+++ src/component/page/DashPage.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import DashLayout from './../layout/DashLayout.jsx';
+import DashSideBar from './../DashSideBar.jsx';
+import PackageSelection from './../PackageSection.jsx'; // Assuming this is your component for the packages
+import CustomizePackage from './../CustomizePackage.jsx';
+
+const DashPage = () => {
+  const [activeSection, setActiveSection] = useState('packages'); // State to manage active section
+
+  return (
+    <DashLayout>
+      <DashSideBar setActiveSection={setActiveSection} /> {/* Pass setActiveSection to the sidebar */}
+      
+      {/* Main content area */}
+      <div className="main-content">
+        {activeSection === 'packages' && <PackageSelection />}
+        {activeSection === 'customizePackage' && <CustomizePackage />}
+        {/* {activeSection === 'bookings' && <Bookings />}  */}
+      </div>
+    </DashLayout>
+  );
+};
+
+export default DashPage;
